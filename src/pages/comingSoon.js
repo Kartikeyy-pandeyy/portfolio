7f9ec762baf77b6a858073f1dd843915e90c99fa
Kartikeyy-pandeyy <kartikeyy.pandeyy@gmail.com>
2025-03-13 03:55:13 +0530

    Comming Soon

---
+++ src/pages/comingSoon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/comingSoon.css';
+
+const ComingSoonPage = () => {
+  return (
+    <div className="coming-soon-container">
+      <h1 className="message">
+        I’ll be back soon, Meanwhile you may check my <a href="https://www.linkedin.com/in/kartikey-pandey-5788aa252/" target="_blank" rel="noopener noreferrer">LinkedIn.</a>
+      </h1>
+    </div>
+  );
+};
+
+export default ComingSoonPage;
