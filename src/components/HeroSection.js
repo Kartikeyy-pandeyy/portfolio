98a6c9d4ce8c720eea419c012b9bbdffaf1f568b
Kartikeyy-pandeyy <kartikeyy.pandeyy@gmail.com>
2025-03-16 03:22:58 +0530

    updating ep

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -18,7 +18,7 @@ const HeroSection = () => {
   useEffect(() => {
     if (!hasFetched.current) {
       hasFetched.current = true;
-      fetch("http://3.110.124.79:5000/api/visitors")
+      fetch("https://3.110.124.79:5000/api/visitors")
         .then((response) => response.json())
         .then((data) => {
           const end = data.count;
